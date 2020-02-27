ef8668d864d7e4787ddd8c70de3dcb72805be2f7
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-02-27 08:38:53 -0300

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    "react/jsx-filename-extension": [1,{"extensions": [".js",".jsx"]}],
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-console": "off",
+    "no-alert": "off",
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import":{
+        rootPathSuffix: "src"
+      }
+    }
+  }
+};
